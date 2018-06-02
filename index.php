3bf57ec72fdc803d48399670b567bffd0e472347
parth <parthjam09@gmail.com>
2018-06-02 15:22:53 +0530

    2-6

--- index.php
+++ index.php
@@ -106,6 +106,7 @@
               <h5 class="card-title">Card title</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               <a href="#" class="btn btn-primary">Add to Cart</a>
+				<a href="#" class="btn btn-success">Buy Now</a>
             </div>
           </div>
         </div>
@@ -145,6 +146,7 @@
         </div>
       </div>
     </div>
+
     <hr>
     <h2 class="text-center">FEATURED PRODUCTS</h2>
     <hr>
